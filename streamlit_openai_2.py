c05c3b4346d7ae1eb031b7a57e841e11c71aa273
kmjune1535 <kmjune1535@gmail.com>
2024-02-28 19:10:27 +0900

    Update streamlit_openai_2.py

--- streamlit_openai_2.py
+++ streamlit_openai_2.py
@@ -2,8 +2,8 @@ from openai import OpenAI
 import streamlit as st
 import time
 
-assistant_id = "asst_KWOuZCh5rk7WlTF8qamsI7Bi"
-# thread_id = "thread_Uf7rGpDc6SoHV1pQKUH5MdsK"
+assistant_id = st.secrets["assistant_id"]
+thread_id = st.secrets["thread_id"]
 
 with st.sidebar:
     st.link_button("테스트 링크", "https://naver.com")
@@ -16,7 +16,7 @@ with st.sidebar:
 
     client = OpenAI(api_key=openai_api_key)
 
-    thread_id = st.text_input("Thread ID", value="thread_Uf7rGpDc6SoHV1pQKUH5MdsK")
+    thread_id = st.text_input("Thread ID", value=thread_id)
 
     thread_make_btn = st.button("Create a new thread")
     if thread_make_btn:
